d8b81ccfd4d87a33a14f68e3e3df827a82559e18
barishu09 <60322682+barishu09@users.noreply.github.com>
2025-03-31 03:54:01 +0530

    build library install

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -17,7 +17,7 @@ function Footer() {
           <h3>Designed and Developed by Rishabh Bansal</h3>
         </Col>
         <Col md="4" className="footer-copywright">
-          <h3>Copyright © {year} VK</h3>
+          <h3>Copyright © {year} RB</h3>
         </Col>
         <Col md="4" className="footer-body">
           <ul className="footer-icons">
